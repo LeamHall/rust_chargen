882c5dfa85005ababe8e57411ceb86a115ff5f7a
Leam Hall <leamhall@gmail.com>
2024-04-05 10:58:14 -0500

    Working on the struct.

--- rust_chargen/src/main.rs
+++ rust_chargen/src/main.rs
@@ -12,45 +12,75 @@
 
 // Imported via the Cargo.toml file
 use rand::Rng;
+// use rand::prelude::SliceRandom;
 
 // Returns an 8 bit unsigned integer.
 // Like Ruby, the last successful command is the return value.
 // Maybe give it a minimum roll option?
-fn roll() -> u8 {
+fn roll() -> i8 {
     rand::thread_rng().gen_range(1..7)
 }
 
 // 2d6 for us Traveller people. Make your own 3d6 for DnD.
 // Needs a modifier.
-fn two_d6() -> u8 {
+fn two_d6() -> i8 {
     roll() + roll()
+}
+
+// Generate gender.
+fn gen_gender() -> String {
+    let options = vec!["m", "f"];
+    let pick = rand::thread_rng().gen_range(0..2);
+    return options[pick].to_string();
+}
+
+// Build the character.
+fn build_character() -> Character {
+    let _name = String::from("Fred");
+    let _gender = gen_gender();
+    let _age = 18;
+    let _stats = gen_stats();
+    let mut character = Character {
+        name: _name,
+        gender: _gender,
+        age: _age,
+        stats: _stats,
+        upp: String::from(""),
+    };
+    return character;
+}
+
+
+// Generate the stats.
+fn gen_stats() -> Vec<i8> {
+    let mut stat_block: Vec<i8> = Vec::new();
+    let mut i = 0;
+    while i < 6 {
+        // I wonder if there's a better way for a limited iteration?
+        i = i + 1;
+        stat_block.push(two_d6());
+    };
+    return stat_block;
 }
 
 // The main function is where things happen.
 fn main() {
-    // This is the old code. It works, but I want to make it better.
-    // New code will start at //**
     // 'mut' means i is mutable, otherwise it would not be.
-    let mut i = 0;
-    while i < 6 {
-        i = i + 1;
-        // The ! means print is a macro. Still learning about those.
-        print!("{} ", two_d6());
-    }
-    // This is to add a newline after the results. If you're doing one line,
-    // you can get a free newline with println!.
-    //     println!("My cool character.")
-    print!("\n")
-
-    // ** This is the new code. Starting with pseudo-code to get things
-    // in my head.
+    // Starting with pseudo-code to get things in my head.
     // 
     // Build a structure (?) to hold all of the data.
     // - Name (string)
     // - Gender (string)
     // - Age (int, but converts to a string)
-    // - UPP (6 ints that get turned into a hexidecimal string character)
+    // - stats (some ints)
+    // - UPP (string calculated from stats)
     //
+    //
+    let character = build_character();
+
+    println!("{} Age: {} Gender: {}  Stats: {:?}", 
+        character.name, character.age, 
+        character.gender, character.stats);
     // Assign a gender (M,F)
     //
     // Get a name from the database. 
@@ -72,7 +102,16 @@ fn main() {
     // Print the collection.
 }
 
-// Here's a test function. The "#[test]" is the declaration.
+// The Character struct
+struct Character {
+    name:   String,
+    gender: String,
+    age:    i8,
+    stats:  Vec<i8>,
+    upp:    String,
+}
+
+// Here are the test functions. The "#[test]" is the declaration.
 // It is called with:
 //      cargo test
 #[test]
@@ -88,4 +127,15 @@ fn test_roll() {
 
 }
 
+#[test]
+fn test_gender() {
+    let mut counter = 0;
+    let mut a: Vec<String> = Vec::new();
+    a.push("m".to_string());
+    a.push("f".to_string());
+    while counter < 100 {
+        counter = counter + 1;
+        assert!(a.contains(&gen_gender()));
+    }
+}
 
